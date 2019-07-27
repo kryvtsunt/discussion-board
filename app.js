cd302d8be4ea3fb50611d00fb7e5bf3006e6a9a9
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-07-27 15:54:26 -0400

    data persistence

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/husky_board");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var items = [
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10},
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10},
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10},
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10},
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10},
-  {name: "Item1", image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420", price: 10}
-];
+// SCHEMA
+var boardItemSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var BoardItem = mongoose.model("BoardItem", boardItemSchema);
+
+// BoardItem.create(
+//   {
+//     name: "First Item",
+//     image: "https://images.contentful.com/q602vtcuu3w3/6CGViz02AM2aOiyWYo4EQ4/6f74f2e5e34679a35ac329f5a194988d/Group_10.jpg?q=80&w=420"
+//   }, function(err, item){
+//     if(err){
+//       console.log(err)
+//     } else {
+//       console.log("Created Item");
+//       console.log(item);
+//     }
+//   }
+// );
 
 
 app.get("/", function(req, res){
@@ -20,16 +35,26 @@ app.get("/", function(req, res){
 });
 
 app.get("/items", function(req, res){
-
-    res.render("items", {items: items});
+    BoardItem.find({}, function(err, items){
+      if (err){
+        console.log(err);
+      } else {
+        res.render("items", {items: items});
+      }
+    })
 });
 
 app.post("/items", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newItem = {name: name, image:image};
-    items.push(newItem);
-    res.redirect("/items");
+    BoardItem.create(newItem, function(err, item){
+      if(err){
+        console.log(err);
+      } else {
+        res.redirect("/items");
+      }
+    })
 
 })
 
