e497cd2f172cd8370ebaf62562fba5e7d14db7fa
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-09-02 16:59:21 -0400

    login routes

--- app.js
+++ app.js
@@ -141,6 +141,17 @@ app.post("/register", function(req, res){
   });
 });
 
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+app.post("/login", passport.authenticate("local",
+{
+  sucesRedirect: "/items",
+  failureRedirect: "/login"
+}), function(req, res){
+});
+
 
 
 app.listen(3000, function(){
