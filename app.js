753fdb7bdcad115b6096bb51cd740dd4d4f6654a
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2019-09-02 18:38:55 -0400

    logout

--- app.js
+++ app.js
@@ -152,6 +152,11 @@ app.post("/login", passport.authenticate("local",
 }), function(req, res){
 });
 
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("campgrounds");
+})
+
 
 
 app.listen(3000, function(){
